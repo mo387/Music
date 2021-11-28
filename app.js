8df5815cc0c681c2df469192495c81ba385509b2
mo <1435740533@qq.com>
2021-11-28 14:29:55 +0800

    更新了文件目录层级

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 import Express from "express";
 const app = Express()
 app.use(Express.static('public'))
-app.get('/', (req, res) => {
+app.all('*', function (req, res, next) {
+
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With,yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  if (req.method == 'OPTIONS') {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+});
+app.get('/api', (req, res) => {
+  console.log('被请求')
   res.send('hello world')
 })
-app.listen(8080)
+app.listen(9999)
